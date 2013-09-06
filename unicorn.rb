c8a9013d1aa48192fd4c1b4ec8395207fcd5f46e
Michael Gannon <michael.gannon@gmail.com>
2013-09-06 15:07:25 +1000

    unicorn config

---
+++ unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/var/www/chronular.com"
+pid "/var/www/chronular.com/tmp/pids/unicorn.pid"
+stderr_path "/var/www/chronular.com/unicorn.log"
+stdout_path "/var/www/chronular.com/unicorn.log"
+
+listen "/var/www/chronular.com/tmp/sockets/unicorn.sock"
+worker_processes 2
+timeout 30
